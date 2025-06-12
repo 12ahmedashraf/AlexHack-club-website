4427739f6052d93e2c640d51ee537701889cdced
Ahmed Ashraf <93441921+12ahmedashraf@users.noreply.github.com>
2025-06-12 22:53:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let x=document.getElementsByClassName("hero-div")[0];
+const bgs=[
+    'url("/pics/bg3.png")', 
+    'url("/pics/bg1.png")',
+    'url("/pics/bg2.png")',
+    'url("/pics/bg4.png")'
+];
+let y=0;
+        document.getElementsByClassName("circle")[0].style.backgroundColor="white";
+
+function change_back(z)
+{
+    y=y+z;
+    let u=(y%bgs.length+bgs.length)%bgs.length;
+    let i=(y%3+3)%3;
+    x.style.backgroundImage = bgs[u];
+    if(i==2)
+    {
+            document.getElementsByClassName("circle")[2].style.backgroundColor="white";
+    document.getElementsByClassName("circle")[0].style.backgroundColor="rgba(255,255,255,0)";
+        document.getElementsByClassName("circle")[1].style.backgroundColor="rgba(255,255,255,0)";
+
+    }
+     if(i==1)
+    {
+            document.getElementsByClassName("circle")[1].style.backgroundColor="white";
+    document.getElementsByClassName("circle")[0].style.backgroundColor="rgba(255,255,255,0)";
+        document.getElementsByClassName("circle")[2].style.backgroundColor="rgba(255,255,255,0)";
+
+    }
+    if(i==0)
+    {
+        document.getElementsByClassName("circle")[0].style.backgroundColor="white";
+    document.getElementsByClassName("circle")[1].style.backgroundColor="rgba(255,255,255,0)";
+        document.getElementsByClassName("circle")[2].style.backgroundColor="rgba(255,255,255,0)";
+    }
+}
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY === 0) {
+        navbar.style.backgroundColor = "rgba(255,255,255,0)"; 
+  } else {
+        navbar.style.backgroundColor = "#08283B";
+  }
+});
+let toggle=0;
+function toggleMenu() {
+    toggle++;
+    const nav = document.querySelector(".links");
+        nav.classList.toggle("show");
+
+  }
